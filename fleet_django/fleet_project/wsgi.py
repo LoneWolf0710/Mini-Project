362f634c51eea8e802f569312fad411248a4255e
pankaj9259 <pankajkumar.925905@gmail.com>
2026-04-28 09:32:31 +0530

    updated

--- fleet_django/fleet_project/wsgi.py
+++ fleet_django/fleet_project/wsgi.py
@@ -2,3 +2,4 @@ import os
 from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'fleet_project.settings')
 application = get_wsgi_application()
+# Contributed by pankaj9259
